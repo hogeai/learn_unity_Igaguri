dac21ebb3617df8424fd632ec4648a9325bda0b3
hogeai <ksk.kai@gmail.com>
2019-03-28 11:33:46 +0900

    first commit

---
+++ Assets/IgaguriController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Shoot(new Vector3(0, 200, 2000));
+    }
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+
+        throw new NotImplementedException();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
